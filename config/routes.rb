aaba329c0f128f12223a90a37ec9ad831eaddc9b
shafrazi <shafrazi@live.com>
2019-01-20 17:17:25 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,52 @@
+Rails.application.routes.draw do
+  get "contact", to: "contacts#new"
+  get 'contacts/create'
+  get "about", to: "about#index"
+  namespace :admin do
+    get 'sessions/new'
+    get 'sessions/create'
+  end
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/new'
+    get 'users/create'
+    get 'users/edit'
+    get 'users/update'
+    get 'users/destroy'
+  end
+  namespace :admin do
+    get 'comments/destroy'
+  end
+  get 'comments/new'
+  namespace :admin do
+    get 'categories/index'
+    get 'categories/show'
+    get 'categories/new'
+    get 'categories/create'
+    get 'categories/edit'
+    get 'categories/update'
+    get 'categories/destroy'
+  end
+  get 'categories/show'
+  namespace :admin do
+    get 'posts/new'
+    get 'posts/create'
+    get 'posts/edit'
+    get 'posts/update'
+    get 'posts/destroy'
+    get 'posts/index'
+    get 'posts/show'
+  end
+  get 'posts/index'
+  get 'posts/show'
+
+  root "posts#index"
+
+  resources :posts, :categories, :comments
+  resources :contacts, only: [:new, :create]
+  namespace :admin do
+    resources :posts, :categories, :comments, :users
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
